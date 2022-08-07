f75fe19317cfde216aec5683c79b66dcf113820c
GuidoMatijas1 <matihas8@gmail.com>
2022-08-07 20:59:04 +0300

    guido

--- project/auth.py
+++ project/auth.py
@@ -43,6 +43,7 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    city = request.form.get('city')
 
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
@@ -51,7 +52,7 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), city=city)
 
     # add the new user to the database
     db.session.add(new_user)
