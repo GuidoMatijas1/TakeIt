7e73f8491c6f0c68cda5c06e2605cd8beaf34a84
GuidoMatijas1 <matihas8@gmail.com>
2022-08-12 12:25:48 +0300

    shit

--- project/main.py
+++ project/main.py
@@ -1,7 +1,8 @@
-from . import db
-from flask import Blueprint, render_template
+from .models import User
+from flask import Blueprint, render_template, url_for
 from flask_login import login_required, current_user
-
+import flask_sqlalchemy as fs
+from . import db
 
 main = Blueprint('main', __name__)
 
@@ -19,7 +20,18 @@ def test():
     return render_template('test.html')
 
 
+@main.route('/user_profile/<email>')
+def user_profile(email):
+    user = User.query.filter_by(email=email).first()
+    return render_template('profile.html', user=user)
+
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+@main.route('/test_page')
+def test_page():
+    user_list = User.query.all()
+    return render_template('test_page.html', items=user_list)
